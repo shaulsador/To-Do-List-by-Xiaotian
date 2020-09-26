76ba72022b09ac617deb5f3debe4998abea729d1
Xiaotian He <shausado@outlook.com>
2020-09-25 20:12:38 -0400

    second change

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const toDoButton = document.querySelector('.todo-button');
 
 //event listener
 toDoButton.addEventListener('click', addItem);
+toDoList.addEventListener('click', deleteItem);
 
 //functions
 function addItem(event) {
@@ -29,6 +30,37 @@ function addItem(event) {
     trash.innerHTML = '<i class="fas fa-trash-alt"></i>';
     trash.classList.add('trash')   
     toDoDiv.appendChild(trash); 
+    //alternative way to delete
+    // trash.addEventListener('click', deleteItem);
+    // function deleteItem(event) {
+    //     toDoDiv.innerHTML = null;
+    // }
+    //append to list
     toDoList.appendChild(toDoDiv);
+    //clear input value
     toDoInput.value = '';
+}
+
+function deleteItem(e) {
+    let onClick = e.target;
+    if(onClick.classList[0] === 'trash') {
+        let trashDiv = onClick.parentElement;
+        //transition
+        trashDiv.classList.add('erased');
+        //alternative way to temove item after transition
+        // p = new Promise(function(resolve, reject) {
+        //     window.setTimeout(function(){
+        //         resolve();
+        //     }, 500
+        //     );
+        // });
+        // p.then(function(){
+        //     trashDiv.remove();
+        // })
+        trashDiv.addEventListener('transitionend', function(){
+            trashDiv.remove();
+        })
+    } else if(onClick.classList[0] === 'checkmark') {
+        onClick.parentElement.classList.toggle('completed');
+    }
 }
