de39568c9977a01b81e1a4de228961ec8c61cb77
Xiaotian He <shausado@outlook.com>
2020-09-26 00:24:04 -0400

    filter update

--- app.js
+++ app.js
@@ -2,16 +2,18 @@
 const toDoList = document.querySelector('.todo-list');
 const toDoInput = document.querySelector('.todo-input');
 const toDoButton = document.querySelector('.todo-button');
+const toDoFilter = document.querySelector('select');
 
 //event listener
 toDoButton.addEventListener('click', addItem);
 toDoList.addEventListener('click', deleteItem);
+toDoFilter.addEventListener('click', filterItem);
 
 //functions
 function addItem(event) {
     //prevent default form submitting
     event.preventDefault();
-    console.log('he');
+    //test: console.log('he');
     //to do div
     const toDoDiv = document.createElement('div');
     toDoDiv.classList.add('todo-div')
@@ -62,5 +64,35 @@ function deleteItem(e) {
         })
     } else if(onClick.classList[0] === 'checkmark') {
         onClick.parentElement.classList.toggle('completed');
+    }
+}
+
+function filterItem(e) {
+    let items = toDoList.childNodes;
+    switch(e.target.value) {
+        case 'all': 
+            items.forEach(function(item) {
+                item.style.display = 'flex';
+            });
+            break;
+        case 'done':
+            items.forEach(function(item) {
+                if(item.classList.contains('completed')) {
+                    item.style.display = 'flex';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+            break;
+        case 'to do':
+            items.forEach(function(item) {
+                if(!item.classList.contains('completed')) {
+                    item.style.display = 'flex';
+                } else {
+                    item.style.display = 'none';
+                }
+            })
+            break;
+
     }
 }
